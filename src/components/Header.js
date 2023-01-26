ee30c8cb3f3ffe060ff53bc05d8266806820d20e
IshKevin <carterk279@gmail.com>
2023-01-26 15:54:32 +0200

    make header title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Button from './Button'
 
-const Header = () => {
+const Header = ({onAdd}) => {
   return (
     <header className='header'>
         <h1>Task tracker</h1>
-        <Button color='green' text='Add' onClick= {onclick} />
+        <Button color='green' text='Add' onClick= {onAdd} />
         
     </header>
   )
